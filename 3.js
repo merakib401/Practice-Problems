0fd7c1cb4a4f59130bd4415293ee1c2fd4c791ed
Rakibul Islam <rakib12401@gmail.com>
2024-06-26 00:01:45 +0600

    leap year or not

---
+++ 3.js
@@ -0,0 +1,15 @@
+//Write a function Whether a year is leap year or not
+
+function isLeapYear(year){
+    if(year%400===0){
+        return true;
+    } else if(year%100===0){
+        return false;
+    }
+    else if(year%4===0){
+        return true;
+    }
+    return false;
+
+}
+console.log(isLeapYear(1770));
